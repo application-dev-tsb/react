d0ad31cf97fd4919d73a57fd9f15c288928fdd4f
LM Bibera <hack.of.all.codes@gmail.com>
2022-05-21 08:16:36 +0800

    add a new form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  const buttonText = 'Click Me!';
+
   return (
     <div style={{ backgroundColor: 'red', border: '1px solid red' }}>
-      Hi there!
+      
+      <label for="name">Enter Name:</label>
+      <input id="name" type="text"/> 
+
+      <button>{buttonText}</button>
     </div>
   );
 }
